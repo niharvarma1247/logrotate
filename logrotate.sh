2891489ab40aca240e2980b80e9c398b01b71d31
root <root@neehar-consul1.0c1n1j0lln3ulppefi0qjyajbg.yx.internal.cloudapp.net>
2016-09-23 06:49:12 +0000

    changed docker run1 cmd

--- logrotate.sh
+++ logrotate.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-#docker run -d -v /var/lib/docker/containers:/var/lib/docker/containers -v /var/log/docker:/var/log/docker -e "LOGS_DIRECTORIES=/var/lib/docker/containers /var/log/docker" -e "LOGROTATE_SIZE=100M" barcond/logrotate:1
+#dockerhbsdhbjsdh run -d -v /var/lib/docker/containers:/var/lib/docker/containers -v /var/log/docker:/var/log/docker -e "LOGS_DIRECTORIES=/var/lib/docker/containers /var/log/docker" -e "LOGROTATE_SIZE=100M" barcond/logrotate:1
 docker run -d -v /var/lib/docker/containers:/var/lib/docker/containers -v /var/log/docker:/var/log/docker -e "LOGS_DIRECTORIES=/var/lib/docker/containers /var/log/docker" -e "LOGROTATE_SIZE=100M" barcond/logrotate:1
